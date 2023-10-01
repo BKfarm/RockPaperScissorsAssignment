e741f6708fa58a9b30f880ac4f65aa70c7b77785
Ben Fahrmeier <144169391+BKfarm@users.noreply.github.com>
2023-10-01 12:46:34 -0400

    release candidate

--- RPS.java
+++ RPS.java
@@ -8,6 +8,7 @@ public class RPS
         boolean finished = false;
         String playA = "";
         String playB = "";
+        String gameEnder = "";
         Scanner in = new Scanner(System.in);
         do
         {
@@ -35,13 +36,50 @@ public class RPS
             switch (playA)
             {
                 case "R":
-
+                    switch (playB)
+                    {
+                        case "R":
+                            System.out.println("Rock vs. rock- It's a tie!");
+                            break;
+                        case "P":
+                            System.out.println("Paper covers rock- Player B wins!");
+                            break;
+                        case "S":
+                            System.out.println("Rock smashes scissors- Player A wins!");
+                    }
+                    break;
                 case "P":
-
+                    switch (playB)
+                    {
+                        case "R":
+                            System.out.println("Paper covers rock- Player A wins!");
+                            break;
+                        case "P":
+                            System.out.println("Paper vs. paper- It's a tie!");
+                            break;
+                        case "S":
+                            System.out.println("Scissors cuts paper- Player B wins!");
+                    }
+                    break;
                 case "S":
-
+                    switch (playB)
+                    {
+                        case "R":
+                            System.out.println("Rock smashes scissors- Player B wins!");
+                            break;
+                        case "P":
+                            System.out.println("Scissors cuts paper- Player A wins!");
+                            break;
+                        case "S":
+                            System.out.println("Scissors vs. scissors- It's a tie!");
+                    }
             }
-
+            System.out.println("Would you like to continue? [Y/N]");
+            gameEnder = in.nextLine();
+            if (gameEnder.equalsIgnoreCase("N"))
+            {
+                finished = true;
+            }
         }while(!finished);
     } 
 }
